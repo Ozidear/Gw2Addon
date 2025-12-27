c614d9671e0b6327ed2c84f754c7c2e8027a1b16
Ozidear <omerru06@gmail.com>
2025-12-27 21:40:11 +0200

    built a simple alert addon

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <httplib.h>
 #include <iostream>
+#include <Nexus.h>
+#include <string>
 
-int main() {
-    httplib::Client client("https://api.farming-community.eu");
+AddonAPI* g_addon_api = nullptr;
+
+void https() {
+    httplib::Client client{"https://api.farming-community.eu"};
 
     auto res = client.Get("/api/v1/details/salvaging/unid-gear/piece-of-rare-unidentified-gear");
     
@@ -13,6 +17,44 @@ int main() {
     } else {
         std::cerr << "Request failed\n";
     }
-
-    return 0;
 }
+
+void handle_keybinds(const char* keybind_identifier, bool release) {
+    std::string keybind_identifier_string = std::string(keybind_identifier);
+
+    if (keybind_identifier_string == "test") {
+        g_addon_api->UI.SendAlert("Test alert!!!");
+    }
+}
+
+void load_addon(AddonAPI* addon_api) {
+    g_addon_api = addon_api;
+
+    g_addon_api->InputBinds.RegisterWithString("test", handle_keybinds, "CTRL+N");
+}
+
+void unload_addon() {
+    // Addon unloading logic here
+}
+
+extern "C" __declspec(dllexport)
+AddonDefinition* GetAddonDef() {
+    return new AddonDefinition{
+        .Signature=-1012,
+        .APIVersion=NEXUS_API_VERSION,
+        .Name="TestAddon",
+        .Version=AddonVersion{1, 0, 0, 0},
+        .Author="Ozidear",
+        .Description="Testing description",
+        .Load=load_addon,
+        .Unload=unload_addon,
+        .Flags=EAddonFlags_None,
+        // .Provider=EUpdateProvider_GitHub,
+        // .UpdateLink="https://github.com/RaidcoreGG/GW2-Compass"
+    };
+}
+
+int main() {
+    https();
+}
+
