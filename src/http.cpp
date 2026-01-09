42c5a11fa6d8f5d5cf9b756af540b2a923ed754c
Ozidear <omerru06@gmail.com>
2026-01-09 14:52:11 +0200

    moved dict to RareUnidData conversion inside get_rare_unid_fast_farming_profits

--- src/http.cpp
+++ src/http.cpp
@@ -18,5 +18,15 @@ RareUnidData get_rare_unid_fast_farming_profits() {
         profit_map[key] = entry["TPSellProfit"].get<float>();
     }
 
-    return RareUnidData::from_unordered_map(profit_map);
+    return RareUnidData{
+        .tp_profit = profit_map.at("Current Price on TP - 1 Piece of Rare Unidentified Gear"),
+        .tp_tax_profit = profit_map.at("Sell on TP (15% taxes) - 1 Piece of Rare Unidentified Gear"),
+        .opened_salvage_profit = profit_map.at(
+            "Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Opened) → 1 Rare Gear → 1 Silver Fed Salvage o Matic"
+        ),
+        .unopened_salvage_profit = profit_map.at("Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Unopened) →  1 Silver Fed Salvage o Matic"),
+        .open_extract_salvage_profit = profit_map.at(
+            "Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Opened) → 1 Rare Gear → 1 Endless Upgrade Extractor → 1 Silver Fed Salvage o Matic"
+        )
+    };
 }
--- src/http.h
+++ src/http.h
@@ -1,28 +1,11 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
-
 struct RareUnidData {
     float tp_profit;
     float tp_tax_profit;
     float opened_salvage_profit;
     float unopened_salvage_profit;
     float open_extract_salvage_profit;
-
-    static RareUnidData from_unordered_map(const std::unordered_map<std::string, float>& map) {
-        return RareUnidData{
-            .tp_profit = map.at("Current Price on TP - 1 Piece of Rare Unidentified Gear"),
-            .tp_tax_profit = map.at("Sell on TP (15% taxes) - 1 Piece of Rare Unidentified Gear"),
-            .opened_salvage_profit = map.at(
-                "Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Opened) → 1 Rare Gear → 1 Silver Fed Salvage o Matic"
-            ),
-            .unopened_salvage_profit = map.at("Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Unopened) →  1 Silver Fed Salvage o Matic"),
-            .open_extract_salvage_profit = map.at(
-                "Piece of Rare Unidentified Gear - 1 Piece of Rare Unidentified Gear (Opened) → 1 Rare Gear → 1 Endless Upgrade Extractor → 1 Silver Fed Salvage o Matic"
-            )
-        };
-    }
 };
 
 RareUnidData get_rare_unid_fast_farming_profits();
