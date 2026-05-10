a3c797408e0c6dc03e651c856ebd99116a668510
Ozidear <omerru06@gmail.com>
2026-05-10 22:37:45 +0300

    fixed json parsing

--- src/http.cpp
+++ src/http.cpp
@@ -2,20 +2,24 @@
 #include <nlohmann/json.hpp>
 #include <string>
 #include <unordered_map>
-using json = nlohmann::json;
 
-#include "http.h"
+#include "http.hpp"
+
+// using json = nlohmann::json;
+typedef nlohmann::json json;
 
 RareUnidData get_rare_unid_fast_farming_profits() {
     httplib::Client client{"https://api.farming-community.eu"};
     httplib::Result res = client.Get("/api/v1/details/salvaging/unid-gear/piece-of-rare-unidentified-gear");
 
+    std::cout << res->body << "\n";
+
     json json_res = json::parse(res->body);
     std::unordered_map<std::string, float> profit_map;
 
-    for (const auto& entry : json_res["list"]) {   
+    for (const json& entry : json_res["list"]) {
         std::string key = entry["Name"].get<std::string>() + " - " + entry["Price"].get<std::string>();
-        profit_map[key] = entry["TPSellProfit"].get<float>();
+        profit_map[key] = entry["TPSellRevenue"].get<float>();
     }
 
     return RareUnidData{
